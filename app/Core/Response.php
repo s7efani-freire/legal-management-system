9d26b7a14cac3ee312ee7a9d4acbb90e77a4f882
s7efani-freire <stefani7freire@gmail.com>
2025-12-17 15:09:14 -0300

    chore(core): padronizar respostas JSON da API

--- app/Core/Response.php
+++ app/Core/Response.php
@@ -7,7 +7,8 @@ final class Response {
   public static function json(array $data, int $status = 200): void {
     http_response_code($status);
     header('Content-Type: application/json; charset=utf-8');
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
